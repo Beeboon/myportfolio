d8db839900a134269c8f56de8f91f02dc7831bd4
Tom Daniaud <tom.daniaud@etu.univ-nantes.fr>
2025-01-12 17:50:41 +0100

    first component ever

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,9 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      fontFamily: {
+        mono: ['"Geist Mono"', 'monospace'],
+      }
     },
   },
   plugins: [],
