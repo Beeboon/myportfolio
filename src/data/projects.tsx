bdb6f43041f26a90270e5830193c687ff863422c
Tom Daniaud <tom.daniaud@etu.univ-nantes.fr>
2025-01-27 19:17:44 +0100

    update projectsCardData

---
+++ src/data/projects.tsx
@@ -0,0 +1,26 @@
+export const cards = [
+    {
+        description: "Academic Project on graph exploration",
+        title: "PolyHash",
+        src: "/polyhashProject.png",
+        ctaText: "GitHub",
+        ctaLink: "https://github.com/TomDaniaud/Polyhash",
+        content:
+            <p>
+                A google 2015 HashCode solution develloped trough/with raw python with my teammate <br /><br />
+                Deep understanding of the data structure and fondamental algorithms mainly about graph exploration
+            </p>
+
+    },
+    {
+        description: "A modern Next.js website",
+        title: "Portfolio",
+        src: "/myPortfolio.png",
+        ctaText: "GitHub",
+        ctaLink: "https://github.com/TomDaniaud/myportfolio",
+        content:
+            <p>
+                A web project to learn web developpement in optic to build web app for companies
+            </p>
+    },
+];
